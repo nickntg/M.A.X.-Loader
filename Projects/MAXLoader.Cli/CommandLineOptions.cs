9ccfbb0d24d6f914442b7a7fe3e22ede0f31b685
n.bitounis <n.bitounis@xe.gr>
2023-05-09 07:22:32 +0300

    Added json, gold, research options to the cli.

--- Projects/MAXLoader.Cli/CommandLineOptions.cs
+++ Projects/MAXLoader.Cli/CommandLineOptions.cs
@@ -4,9 +4,15 @@ namespace MAXLoader.Cli
 {
 	public class CommandLineOptions
 	{
-		[Option('i', "input", Required = true, HelpText = "M.A.X. game file to read")]
+		[Option('i', "input", Required = true, HelpText = "M.A.X. game file to read.")]
 		public string GameFile { get; set; }
-		[Option('o', "output", Required = false, HelpText = "Name of file to save")]
+		[Option('o', "output", Required = false, HelpText = "Name of file to save.")]
 		public string GameOutputFile { get; set; }
+		[Option('g', "gold", Required = false, HelpText = "Give 30k of gold to the Red team.")]
+		public bool Gold { get; set; }
+		[Option('r', "research", Required = false, HelpText = "Research topics for the Red team.")]
+		public bool Research { get; set; }
+		[Option('j', "json",Required  = false, HelpText = "Output the save file in JSON format.")]
+		public bool Json { get; set; }
 	}
 }
--- Projects/MAXLoader.Cli/Program.cs
+++ Projects/MAXLoader.Cli/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using CommandLine;
 using MAXLoader.Core.Services;
+using MAXLoader.Core.Services.Interfaces;
 using MAXLoader.Core.Types;
 using MAXLoader.Core.Types.Enums;
 using Newtonsoft.Json;
@@ -19,6 +20,98 @@ namespace MAXLoader.Cli
 
 			var results = Parser.Default.ParseArguments<CommandLineOptions>(args);
 
+			ValidateParameters(results);
+
+			var loader = new GameLoader(new ByteHandler());
+			
+			var game = ReadGameFile(loader, results.Value);
+			
+			var changed = PerformChanges(game, results.Value);
+
+			if (changed)
+			{
+				SaveChanges(game, loader, results.Value);
+			}
+
+			ShowJson(game, results.Value);
+		}
+
+		private static GameFile ReadGameFile(IGameLoader loader, CommandLineOptions options)
+		{
+			GameFile game = null;
+
+			try
+			{
+				game = loader.LoadGameFile(SaveFileType.SinglePlayerCustomGame, options.GameFile);
+			}
+			catch (Exception ex)
+			{
+				Log.Error($"Error reading game file: {ex.Message}");
+				
+				Environment.Exit(1);
+			}
+
+			return game;
+		}
+
+		private static void ShowJson(GameFile game, CommandLineOptions options)
+		{
+			if (options.Json)
+			{
+				Console.WriteLine(JsonConvert.SerializeObject(game, Formatting.Indented,
+					new Newtonsoft.Json.Converters.StringEnumConverter()));
+			}
+		}
+
+		private static void SaveChanges(GameFile game, 
+			IGameLoader loader,
+			CommandLineOptions options)
+		{
+			var outputFile = string.IsNullOrEmpty(options.GameOutputFile)
+				? options.GameOutputFile
+				: options.GameFile;
+
+			try
+			{
+				loader.SaveGameFile(game, outputFile);
+			}
+			catch (Exception ex)
+			{
+				Log.Error(ex, $"Could not save game file to {outputFile}");
+			}
+		}
+
+		private static bool PerformChanges(GameFile game, CommandLineOptions options)
+		{
+			var changed = false;
+
+			if (options.Gold)
+			{
+				Log.Info("Giving 30k gold to the Red team");
+
+				game.TeamUnits[Team.Red].Gold = 30000;
+
+				changed = true;
+			}
+
+			if (options.Research)
+			{
+				Log.Info("Setting all topics to 90% researched with 1 round left to go to 100%");
+
+				for (var i = ResearchTopic.Attack; i <= ResearchTopic.Cost; i++)
+				{
+					game.TeamInfos[Team.Red].ResearchTopics[i].ResearchLevel = 9;
+					game.TeamInfos[Team.Red].ResearchTopics[i].TurnsToComplete = 1;
+				}
+
+				changed = true;
+			}
+
+			return changed;
+		}
+
+		private static void ValidateParameters(ParserResult<CommandLineOptions> results)
+		{
 			if (results.Errors != null && results.Errors.Any())
 			{
 				foreach (var error in results.Errors)
@@ -26,25 +119,10 @@ namespace MAXLoader.Cli
 					if (error.Tag == ErrorType.MissingRequiredOptionError)
 					{
 						Log.Error("Missing required options");
-						return;
+						Environment.Exit(1);
 					}
 				}
 			}
-
-			var loader = new GameLoader(new ByteHandler());
-			GameFile game;
-
-			try
-			{
-				game = loader.LoadGameFile(SaveFileType.SinglePlayerCustomGame, results.Value.GameFile);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"Error reading game file: {ex.Message}");
-				return;
-			}
-
-			Console.WriteLine(JsonConvert.SerializeObject(game, Formatting.Indented, new Newtonsoft.Json.Converters.StringEnumConverter()));
 		}
 
 		private static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
